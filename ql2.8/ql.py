f029697a0d6556312427993ebf5f11db8d45b3a3
zxcll <85672694+zxcll@users.noreply.github.com>
2021-07-28 17:22:01 +0800

    Update ql.py

--- ql2.8/ql.py
+++ ql2.8/ql.py
@@ -25,7 +25,7 @@ def qltoken():
 
 # 查询并获取京东cookie
 def cxck(token, okl_token,cookies):
-    jd_ua = "jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36"
+    jd_ua = "JD4iPhone/167724 (iPhone; iOS 15.0; Scale/3.00)Accept-Language: zh-Hans-CN;q=1"
     t = round(time.time())
     url = 'https://plogin.m.jd.com/cgi-bin/m/tmauthchecktoken?&token={0}&ou_state=0&okl_token={1}'.format(token,okl_token)
     body1 = {
